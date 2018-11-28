a74ac9229c24ab7fd95ace1ef1842ba779d61581
zipang <christophe.desguez@gmail.com>
2018-11-28 15:27:39 +0100

    Demo of an express REST service for todos

---
+++ src/express/README.md
@@ -0,0 +1,4 @@
+TODOS CRUD
+==========
+
+This is a demo implementation of a simple CRUD REST service for todos.
---
+++ src/express/server.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json());
+
+let todos = [{id: 'jkhsdjkf', content: 'review this code'}];
+
+app.post('/todos', (req, res) => {
+	todos.push({
+		...req.body,
+		id: Math.random().toString(32).slice(2)
+	});
+	res.sendStatus(201);
+});
+
+app.put('/todos/:id', (req, res) => {
+	todos[Number(req.params.id)] = req.body;
+	res.sendStatus(200);
+});
+
+app.get('/todos/:id', (req, res) => {
+	res.send(todos[id]);
+});
+
+app.get('/todos/all', (req, res) => {
+	res.send(todos);
+});
+
+app.get('/', (req, res) => {
+	res.send('OK');
+});
+
+app.listen(8080, () => {
+	console.log('Listening on port 8080...');
+});
