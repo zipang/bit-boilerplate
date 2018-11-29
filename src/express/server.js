40ccfb94279c75ea2f95d735335d162594d68b11
zipang <christophe.desguez@gmail.com>
2018-11-29 10:49:24 +0100

    Use a map as the in-memory data store and add/retrieve data as expected by their ids

--- src/express/server.js
+++ src/express/server.js
@@ -6,27 +6,35 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const todos = [{id: 'jkhsdjkf', content: 'review this code'}];
+// let's use a Map as the in memory key-value data store
+const todos = new Map();
+todos.set('code-review', { id: 'code-review', content: 'review this code' });
 
 app.post('/todos', (req, res) => {
-	todos.push({
-		...req.body,
-		id: Math.random().toString(32).slice(2)
-	});
-	res.sendStatus(201);
+
+	const newTodo = Object.assign({
+		// if the object allready has an id, it will be overwritten
+		id: Date.now().toString(32)
+	}, req.body);
+
+	todos.set(newTodo.id, newTodo);
+
+	// Tell the client where the new resource is located
+	res.setHeader('Location', new URL(`/todos/${newTodo.id}`, 'http://localhost:8080'));
+	res.status(201).send(newTodo);
 });
 
 app.put('/todos/:id', (req, res) => {
-	todos[Number(req.params.id)] = req.body;
+	todos.set(req.params.id, req.body);
 	res.sendStatus(200);
 });
 
 app.get('/todos/:id', (req, res) => {
-	res.send(todos[id]);
+	res.send(todos.get(req.params.id));
 });
 
 app.get('/todos/all', (req, res) => {
-	res.send(todos);
+	res.send(Array.from(todos.values()));
 });
 
 app.get('/', (req, res) => {
