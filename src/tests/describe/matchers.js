ec332344d393ba7be5766afc1a3fac032ec5264e
zipang <christophe.desguez@gmail.com>
2018-12-06 19:08:50 +0100

    Added more tests for the new matchers

--- src/tests/describe/matchers.js
+++ src/tests/describe/matchers.js
@@ -4,13 +4,16 @@ const assert = require("assert");
  * Implements a subset of Jest expect API using only node js assert
  * @see https://jestjs.io/docs/en/expect
  * @see https://nodejs.org/api/assert.html
- * @param   {String} description the assertion context
- * @returns {Object} containing all the assertions methods
+ * @param  {Object} result is the object we want to assert on
+ * @param  {String} description the assertion context
+ * @return {Assertable} a wrapped result object on which we can make assertions
  */
 const assertionsFor = (result, description) => ({
 	toBe: (expected) => assert.strictEqual(result, expected, description),
 	toEqual: (expected) => assert.deepStrictEqual(result, expected, description),
 	toThrow: (expected) => assert.throws(result, expected, description),
+	toBeDefined: () => assert(result !== undefined, description),
+	toBeUndefined: () => assert(result === undefined, description),
 	toContain: (expected) => {
 		if (result && typeof result.indexOf === "function") { // String and Arrays have this method
 			assert(result.indexOf(expected) !== -1, description);
@@ -18,6 +21,12 @@ const assertionsFor = (result, description) => ({
 			assert(false, description);
 		}
 	},
+	toHaveLength: (expected) => assert(typeof result.length === 'number' && result.length === expected, description),
+	toHaveMethods: (expected) => {
+		assert(Array.isArray(expected), `Expect an array of method names`);
+		const missingMethods = expected.filter(methodName => typeof result[methodName] !== 'function');
+		assert(missingMethods.length === 0, `Object doesn't have methods '${missingMethods}'`);
+	}
 })
 
 module.exports = assertionsFor;
